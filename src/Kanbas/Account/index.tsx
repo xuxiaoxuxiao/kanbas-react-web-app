7ebad17747095c5764236362c38b43250973fed9
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-09-12 23:16:34 -0700

    3.4 completed

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -13,10 +13,10 @@ export default function Account() {
                 </td>
                 <td valign="top">
                     <Routes>
-                        <Route path="/"element={<Navigate to="/Kanbas/Account/Signin" />} />
-                    <Route path="/Signin" element={<Signin />} />
-                    <Route path="/Profile" element={<Profile />} />
-                    <Route path="/Signup" element={<Signup />} />
+                      <Route path="/"element={<Navigate to="/Kanbas/Account/Signin" />} />
+                      <Route path="/Signin" element={<Signin />} />
+                      <Route path="/Profile" element={<Profile />} />
+                      <Route path="/Signup" element={<Signup />} />
                     </Routes>
                 </td>
 
