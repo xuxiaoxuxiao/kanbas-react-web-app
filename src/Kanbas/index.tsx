991445f7139ee6205b0a73491e24ea61b2174d78
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-09-18 23:44:35 -0700

    landing page changed & kanbas added

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -6,6 +6,7 @@ import Courses from "./Courses";
 export default function Kanbas() {
     return (
       <div id="wd-kanbas">
+        <h1>Kanbas</h1>
         <table>
           <tr>
             <td valign="top">
