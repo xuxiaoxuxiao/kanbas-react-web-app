d54e623a80a1bdc99abbc5a4efe164d3e0670f0c
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-09-21 19:58:29 -0700

    3.1.4

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,8 +1,38 @@
+import "./index.css";
 export default function Lab2() {
-    return (
-      <div>
-        <h2>Lab 2</h2>
+  return (
+    <div id="wd-lab2">
+      <h2>Lab 2 - Cascading Style Sheets</h2>
+      <h3>Styling with the STYLE attribute</h3>
+      {/* <p style={{ backgroundColor: "blue",
+                  color: "white" }}> */}
+        <p>
+        Style attribute allows configuring look and feel
+        right on the element. Although it's very convenient
+        it is considered bad practice and you should avoid
+        using the style attribute
+      </p>
+
+      <div id="wd-css-id-selectors">
+        <h3>ID selectors</h3>
+        <p id="wd-id-selector-1">
+        Instead of changing the look and feel of all the elements of the same name, e.g., P, we can refer to a specific element by its ID
+        </p>
+        <p id="wd-id-selector-2">
+        Here's another paragraph using a different ID and a different look and feel
+        </p>
       </div>
-    );
-  }
-  
+
+      <div id="wd-css-class-selectors">
+        <h3>Class selectors</h3>
+        <p className="wd-class-selector">
+        Instead of using IDs to refer to elements, you can use an element's CLASS attribute
+        </p>
+        <h4 className="wd-class-selector">
+        This heading has same style as paragraph above
+        </h4>
+      </div>
+
+    </div>
+  );
+}
