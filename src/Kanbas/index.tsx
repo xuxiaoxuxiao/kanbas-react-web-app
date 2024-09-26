b1a7697e28a8d7d1e53cb126b2d6e517e116899e
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-09-25 21:56:47 -0700

    finished 4.1

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -3,29 +3,53 @@ import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
-export default function Kanbas() {
-    return (
-      <div id="wd-kanbas">
-        <h1>Kanbas</h1>
-        <table>
-          <tr>
-            <td valign="top">
-              <KanbasNavigation />
-            </td>
-            <td valign="top">
-              <Routes>
-                <Route path="/" element={<Navigate to="/Kanbas/Account" />} />
-                <Route path="/Account/*" element={<Account />} />
-                <Route path="/Dashboard" element={<Dashboard />} />
-                <Route path="/Courses/:cid/*" element={<Courses />} />
-                <Route path="/Calendar" element={<h1>Calendar</h1>} />
-                <Route path="/Inbox" element={<h1>Inbox</h1>} />
-              </Routes>
-            </td>
-          </tr>
-        </table>
-      </div>
+import "./styles.css";
+import Labs from "../Labs";
 
+export default function Kanbas() {
+  return (
+    <div id="wd-kanbas">
+      <KanbasNavigation />
+      <div className="wd-main-content-offset p-3">     
+        <td valign="top">
+               <Routes>
+                 <Route path="/" element={<Navigate to="/Kanbas/Account" />} />
+                 <Route path="/Account/*" element={<Account />} />
+                 <Route path="/Dashboard" element={<Dashboard />} />
+                 <Route path="/Courses/:cid/*" element={<Courses />} />
+                 <Route path="/Calendar" element={<h1>Calendar</h1>} />
+                 <Route path="/Inbox" element={<h1>Inbox</h1>} />
+                 <Route path="/Labs/*" element={<Labs />} />
+               </Routes>
+          </td>
+        </div>
+    </div>
 
 );}
+
+// export default function Kanbas() {
+//     return (
+//       <div id="wd-kanbas">
+//         <h1>Kanbas</h1>
+//         <table>
+//           <tr>
+//             <td valign="top">
+//               <KanbasNavigation />
+//             </td>
+//             <td valign="top">
+//               <Routes>
+//                 <Route path="/" element={<Navigate to="/Kanbas/Account" />} />
+//                 <Route path="/Account/*" element={<Account />} />
+//                 <Route path="/Dashboard" element={<Dashboard />} />
+//                 <Route path="/Courses/:cid/*" element={<Courses />} />
+//                 <Route path="/Calendar" element={<h1>Calendar</h1>} />
+//                 <Route path="/Inbox" element={<h1>Inbox</h1>} />
+//               </Routes>
+//             </td>
+//           </tr>
+//         </table>
+//       </div>
+
+
+// );}
   
