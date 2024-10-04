de6cefcd8fb24d40b829a2f5c4095f360d8ac424
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-10-04 16:27:41 -0700

    space fixed

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 export default function AccountNavigation() {
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to="/Kanbas/Account/Signin"  className="list-group-item active border border-0" > Signin  </Link> <br/>
-      <Link to="/Kanbas/Account/Signup" className="list-group-item text-danger border border-0"  > Signup  </Link> <br/>
-      <Link to="/Kanbas/Account/Profile" className="list-group-item text-danger border border-0"> Profile </Link> <br/>
+      <Link to="/Kanbas/Account/Signin"  className="list-group-item active border border-0" > Signin  </Link> 
+      <Link to="/Kanbas/Account/Signup" className="list-group-item text-danger border border-0"  > Signup  </Link> 
+      <Link to="/Kanbas/Account/Profile" className="list-group-item text-danger border border-0"> Profile </Link>
     </div>
 );}
