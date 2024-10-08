e07aebb5477dd9e33d0874362216dfd3c73fd1b0
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-10-08 00:04:41 -0700

    finished 2.4.8

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,8 +1,53 @@
+import { IoMdPlanet } from "react-icons/io";
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import IfElse from "./IfElse";
+import LegacyFunctions from "./LegacyFunctions";
+import TernaryOperator from "./TernaryOperator";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import ImpliedReturn from "./ImpliedReturn";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+
+
 export default function Lab3() {
-    return (
-      <div>
-        <h2>Lab 3</h2>
-      </div>
-    );
-  }
-  
+  return(
+    <div id="wd-lab3" className="ms-3">
+      <h3>Lab 3</h3><br/ >
+      <h4>Java Script</h4>
+      <VariablesAndConstants/>
+      <VariableTypes/>
+      <BooleanVariables/>
+      <IfElse/>
+      <TernaryOperator/>
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions/>
+      <ImpliedReturn />
+      <SimpleArrays />
+      <ArrayIndexAndLength/>
+      <AddingAndRemovingToFromArrays/>
+      <ForLoops/>
+      <MapFunction/>
+      <FindFunction/>
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+
+    </div>
+
+    
+
+  );
+}
