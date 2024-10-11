fa35cd4c325900e942963ad9cdfe1b1032608153
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-10-10 20:16:53 -0700

    finished section2

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -18,9 +18,23 @@ import FindFunction from "./FindFunction";
 import FindIndex from "./FindIndex";
 import FilterFunction from "./FilterFunction";
 import JsonStringify from "./JsonStringify";
-
+import House from "./House";
+import TodoItem from "./TodoItem";
+import TodoList from "./TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
+import Square from "../Lab2/Square";
+import Highlight from "./Highlight";
+import AddPathParameters from "./AddPathParameters";
+import PathParameters from "./PathParameters";
 
 export default function Lab3() {
+  console.log('Hello World!');
   return(
     <div id="wd-lab3" className="ms-3">
       <h3>Lab 3</h3><br/ >
@@ -44,6 +58,26 @@ export default function Lab3() {
       <FindIndex />
       <FilterFunction />
       <JsonStringify />
+      <House/>
+      <TodoItem/>
+      <TodoList/>
+      <Spreading/>
+      <Destructing/>
+      <FunctionDestructing/>
+      <DestructingImports/>
+      <Classes/>
+      <Styles /> <hr />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+     </Highlight>
+     <PathParameters />
+     
+
 
     </div>
 
