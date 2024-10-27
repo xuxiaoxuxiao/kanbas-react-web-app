fade38012cde517504f1f162e5f89f78c85b5413
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-10-27 13:11:36 -0700

    3.3.3 finished

--- src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+++ src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
@@ -1,9 +1,17 @@
+import { FaTrash } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
 import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "./GreenCheckmark";
 import Plusmark from "./Plusmark";
-export default function ModuleControlButtons() {
+export default function ModuleControlButtons(
+  { moduleId, deleteModule,editModule}: { 
+    moduleId: string; deleteModule: (moduleId: string) => void;
+    editModule: (moduleId: string) => void }
+) {
   return (
-    <div className="float-end">       
+    <div className="float-end">
+        <FaPencil onClick={() => editModule(moduleId)} className="text-primary me-3" />
+        <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteModule(moduleId)}/>       
         <GreenCheckmark />
         <Plusmark />
         <IoEllipsisVertical className="fs-4" />
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -4,21 +4,55 @@ import LessonControlButtons from "./LessonControlButtons";
 import { BsGripVertical } from "react-icons/bs";
 import { useParams } from "react-router";
 import * as db from "../../Database";
+import React, { useState } from "react";
 
 export default function Modules() {
   const { cid } = useParams();
-  const modules = db.modules;
+  const [modules, setModules] = useState<any[]>(db.modules);
+  const [moduleName, setModuleName] = useState("");
+  const addModule = () => {
+    setModules([ ...modules, { _id: new Date().getTime().toString(),
+                                     name: moduleName, course: cid, lessons: [] } ]);
+    setModuleName("");
+  };
+  const deleteModule = (moduleId: string) => {
+    setModules(modules.filter((m) => m._id !== moduleId));
+  };
+
+  const editModule = (moduleId: string) => {
+    setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));  };
+    const updateModule = (module: any) => {
+      setModules(modules.map((m) => (m._id === module._id ? module : m)));
+  };
     return (
       <div>
         <div>
-          <ModulesControls /><br /><br /><br /><br />
+          
+          <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule} /><br /><br /><br /><br />
           <ul id="wd-modules" className="list-group rounded-0">
             {modules
             .filter((module: any) => module.course === cid)
             .map((module: any) => (
             <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
               <div className="wd-title p-3 ps-2 bg-secondary">
-                <BsGripVertical className="me-2 fs-3" /> {module.name} <ModuleControlButtons />
+              <BsGripVertical className="me-2 fs-3" /> 
+                {!module.editing && module.name}
+                { module.editing && (
+                  <input className="form-control w-50 d-inline-block"
+                    onChange={(e) => updateModule({ ...module, name: e.target.value })}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        updateModule({ ...module, editing: false });
+                      }
+                    }}
+               defaultValue={module.name}/>
+              )}
+
+                
+                <ModuleControlButtons  
+                  moduleId={module._id}
+                  deleteModule={deleteModule}
+                  editModule={editModule}/>
               </div>
               {module.lessons && (
                 <ul className="wd-lessons list-group rounded-0">
