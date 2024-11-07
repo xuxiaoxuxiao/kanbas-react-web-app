a50d07e9138c12a4e37736b1d317add62cb6cd19
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-11-06 16:22:58 -0800

    lab5 added

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,7 +1,7 @@
 export default function Lab5() {
     return (
-      <div id="wd-lab5">
-        <h2>Lab 5</h2>
+      <div id="wd-lab5" className="ms-3">
+        <h3>Lab 5</h3>
         <div className="list-group">
           <a href="http://localhost:4000/lab5/welcome"          
              className="list-group-item">
