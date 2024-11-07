5084268226131baf1c14f5795b2deea729149bda
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2024-11-06 23:34:56 -0800

    3.3 remote objects finished

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,13 +1,21 @@
+import EnvironmentVariables from "./EnvironmentVariables";
+import PathParameters from "./PathParameters";
+import QueryParamenters from "./QueryParameters";
+import WorkingWithObjects from "./WorkingWithObjects";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
     return (
       <div id="wd-lab5" className="ms-3">
         <h3>Lab 5</h3>
         <div className="list-group">
-          <a href="http://localhost:4000/lab5/welcome"          
-             className="list-group-item">
+          <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
              Welcome
           </a>
         </div><hr/>
+        <EnvironmentVariables />
+        <PathParameters />
+        <QueryParamenters />
+        <WorkingWithObjects />
       </div>
     );
   }
